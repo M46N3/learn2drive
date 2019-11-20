23eee88d0544e5c8ee4706697fb47abbf55e4cde
Magne Worren <magneworren@gmail.com>
2019-11-20 22:17:04 +0100

    Added plots for PG run 1

--- plot_graph_average.py
+++ plot_graph_average.py
@@ -11,14 +11,14 @@ window = int(sys.argv[3])
 
 def plotLearning(scores, filename, x=None, window=5):   
     N = len(scores)
-    scores_array = np.empty(N)
-    for i in range(N):
-        scores_array[i] = scores[i]
+    running_avg = np.empty(N)
+    for t in range(N):
+	    running_avg[t] = np.mean(scores[max(0, t-window):(t+1)])
     if x is None:
         x = [i for i in range(N)]
     plt.ylabel('Score')       
     plt.xlabel('Episode')                     
-    plt.plot(x, scores_array)
+    plt.plot(x, running_avg)
     plt.savefig(filename)
 
 
@@ -27,9 +27,8 @@ data = np.loadtxt(filename_input, delimiter=",")
 print(data)
 
 scores = list(data[:,1])
-#print("fsdfs", scores)
+print("fsdfs", scores)
 #print("Filename: ", filename)
-#print("Window: ", window)
+print("Window: ", window)
 
 plotLearning(scores, filename_output, window=window)
-print("%s plotted to file: %s, with size: %d" % (filename_input, filename_output, window))
